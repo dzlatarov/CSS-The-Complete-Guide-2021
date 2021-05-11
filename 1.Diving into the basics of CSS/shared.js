98184617dcef1bbe0b45344edb2f5b44ee32b74b
Danail Zlatarov <danzlatarov1@gmail.com>
2021-05-11 14:56:51 +0300

    manipulating element classes

--- 1.Diving into the basics of CSS/shared.js
+++ 1.Diving into the basics of CSS/shared.js
@@ -7,24 +7,36 @@ var mobileNav = document.querySelector('.mobile-nav');
 
 for (var i = 0; i < planButtons.length; i++) {
     planButtons[i].addEventListener('click', () => {
-        backdrop.style.display = 'block';
-        modal.style.display = 'block';
+        // backdrop.style.display = 'block';
+        // modal.style.display = 'block';
+        // backdrop.className = 'open'; this will overwrite the complete class list
+
+        backdrop.classList.add('open');
+        modal.classList.add('open');
     });
 };
 
 backdrop.addEventListener('click', () => {
-    mobileNav.style.display = 'none';
+    // mobileNav.style.display = 'none';
+
+    mobileNav.classList.remove('open');
     closeModal();
 });
 
 modalNegativeAction.addEventListener('click', closeModal);
 
 function closeModal() {
-    backdrop.style.display = 'none';
-    modal.style.display = 'none';
+    // backdrop.style.display = 'none';
+    // modal.style.display = 'none';
+
+    backdrop.classList.remove('open');
+    modal.classList.remove('open');
 };
 
 toggleButton.addEventListener('click', () => {
-    mobileNav.style.display = 'block';
-    backdrop.style.display = 'block';
+    // mobileNav.style.display = 'block';
+    // backdrop.style.display = 'block';
+
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
 });
