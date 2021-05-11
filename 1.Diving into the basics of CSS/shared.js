0136f066b77fae12ee3e728cefe39f84f5380e38
Danail Zlatarov <danzlatarov1@gmail.com>
2021-05-11 14:44:59 +0300

    opening and closing the menu

--- 1.Diving into the basics of CSS/shared.js
+++ 1.Diving into the basics of CSS/shared.js
@@ -2,6 +2,8 @@ var backdrop = document.querySelector('.backdrop');
 var modal = document.querySelector('.modal');
 var planButtons = document.querySelectorAll('.plan button');
 var modalNegativeAction = document.querySelector('.modal__action--negative');
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
 
 for (var i = 0; i < planButtons.length; i++) {
     planButtons[i].addEventListener('click', () => {
@@ -10,7 +12,10 @@ for (var i = 0; i < planButtons.length; i++) {
     });
 };
 
-backdrop.addEventListener('click', closeModal)
+backdrop.addEventListener('click', () => {
+    mobileNav.style.display = 'none';
+    closeModal();
+});
 
 modalNegativeAction.addEventListener('click', closeModal);
 
@@ -18,3 +23,8 @@ function closeModal() {
     backdrop.style.display = 'none';
     modal.style.display = 'none';
 };
+
+toggleButton.addEventListener('click', () => {
+    mobileNav.style.display = 'block';
+    backdrop.style.display = 'block';
+});
