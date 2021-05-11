ee2ad28282604396a8b100dc68e6041fbaa5588d
Danail Zlatarov <danzlatarov1@gmail.com>
2021-05-11 10:40:27 +0300

    adding an event listener

--- 1.Diving into the basics of CSS/shared.js
+++ 1.Diving into the basics of CSS/shared.js
@@ -1,3 +1,20 @@
 var backdrop = document.querySelector('.backdrop');
+var modal = document.querySelector('.modal');
+var planButtons = document.querySelectorAll('.plan button');
+var modalNegativeAction = document.querySelector('.modal__action--negative');
 
-backdrop.style.display = 'block';
+for (var i = 0; i < planButtons.length; i++) {
+    planButtons[i].addEventListener('click', () => {
+        backdrop.style.display = 'block';
+        modal.style.display = 'block';
+    });
+};
+
+backdrop.addEventListener('click', closeModal)
+
+modalNegativeAction.addEventListener('click', closeModal);
+
+function closeModal() {
+    backdrop.style.display = 'none';
+    modal.style.display = 'none';
+};
